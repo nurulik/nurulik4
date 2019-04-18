a0989a7b1b498c2229228c8c59b9dc5573bf74fe
nurulik <43546341+nurulik@users.noreply.github.com>
2019-04-18 16:43:30 +0700

    tugas keenam

--- nurul-sw.js
+++ nurul-sw.js
@@ -1,17 +1,54 @@
-//proses installasi service worker, pada console akan ditampilkan "Service	worker	installing..."
+//cache disimpan dengan nama "nurul-cache"
+var	CACHE_NAME	=	'nurul-cache';
+//file yang di cache
+var	urlsToCache	=	[
+		'.',
+		'nav.html',
+		'index.html',
+		'css/style.css',
+		'css/materialize.min.css',
+		'pages/about.html',
+		'pages/contact.html',
+		'pages/home.html',
+		'pages/gallery.html'
+];
+
+//proses installasi service worker, 
 self.addEventListener('install',	function(event)	{
-		console.log('Service	worker	installing...');
-		
-//untuk melakukan skip waiting otomatis ketika terdapat perubahan pada service worker
-self.skipWaiting();
+		event.waitUntil(
+				caches.open(CACHE_NAME)
+				.then(function(cache)	{
+						return	cache.addAll(urlsToCache);
+				})
+		);
 });
 
-//proses aktivasi service worker, pada console akan ditampilkan "Service	worker	activating..."
-self.addEventListener('activate',	function(event)	{
-		console.log('Service	worker	activating...');
-});
-
-//untuk mengambil data dari url tertentu
+//mencoba untuk menyocokkan permintaan dengan konten cache. jika sumber daya terdapat di cache, maka dikembalikkan ke asli/awal. 
 self.addEventListener('fetch',	function(event)	{
-		console.log('Fetching:',	event.request.url);
+		event.respondWith(
+				caches.match(event.request)
+				.then(function(response)	{
+						return	response	||	fetchAndCache(event.request);
+				})
+		);
 });
+
+function	fetchAndCache(url)	{
+		return	fetch(url)
+		.then(function(response)	{
+				//mengecek apakah didapatkan respon yang benar. jika error, maka akan ditampilkan pesan error pada console log 
+				if	(!response.ok)	{
+						throw	Error(response.statusText);
+				}
+				//jika respon yang didapatkan benar, maka respon berupa data yang di cache akan disalin lalu disimpan di "nurulindah-cache" dan respon dikembalikan ke yang asli / awal
+				return	caches.open(CACHE_NAME)
+				.then(function(cache)	{
+						cache.put(url,	response.clone());
+						return	response;
+				});
+		})
+		.catch(function(error)	{
+				//jika error, maka akan ditampilkan pesan error pada console log
+				console.log('Request	failed:',	error);
+		});
+}
